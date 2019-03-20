be2263e3c44c40b96fdb588ab5871912cb7756c6
Abdulmohsin ALsharhan <mohsin.sharhan@gmail.com>
2019-03-20 10:56:19 +0300

    bolg done

---
+++ blog/db/migrations/20190320062509-create-comment.js
@@ -0,0 +1,38 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('comments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      body: {
+        type: Sequelize.TEXT
+      },
+      commenter: {
+        type: Sequelize.STRING
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      post_id: {
+        type: Sequelize.INTEGER,
+        onDelete: 'CASCADE',
+        refrences: {
+          model: 'posts',
+          Key: 'id'
+        }
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('comments');
+  }
+};
---
+++ blog/db/models/comment.js
@@ -0,0 +1,15 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Comment = sequelize.define('Comment', {
+    body: DataTypes.TEXT,
+    commenter: DataTypes.STRING
+  }, {tableName: "comments"}
+  );
+  Comment.associate = function(models) {
+    Comment.belongsTo(models.Post, {
+      foreignKey: "post_id",
+      onDelete: "CASCADE"
+    })
+  };
+  return Comment;
+};
